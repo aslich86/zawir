794964640fd50edf166498736a864363988aa094
aslich86 <iwan.aslich@yahoo.com>
2025-12-06 12:56:11 +0700

    chore: update Next.js to patched secure version

--- .next/dev/static/chunks/app_layout_tsx_78cdd4a3._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__51d697a8._.css",
-  "static/chunks/node_modules_97dced73._.js",
-  "static/chunks/_37ba318a._.js"
-],
-    source: "dynamic"
-});
--- .next/dev/static/chunks/app_zakat_page_tsx_79d85ab4._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/components_b56e73fe._.js",
-  "static/chunks/node_modules_476c5845._.js"
-],
-    source: "dynamic"
-});
